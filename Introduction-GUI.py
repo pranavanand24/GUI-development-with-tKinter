99c164168f6f4a306d0e3c9f0da4ee8bd7c0fe32
Pranav Anand <42691222+pranavanand24@users.noreply.github.com>
2020-01-04 17:17:06 +0530

    Add files via upload

---
+++ Introduction-GUI.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+
+root = Tk()
+theLabel = Label(root, text="First Gui interface")
+
+theLabel.pack()
+root.mainloop()
+
---
+++ Organising your Layout-GUI.py
@@ -0,0 +1,23 @@
+from Tkinter import *
+
+root = Tk()
+
+topFrame =  Frame(root)
+topFrame.pack()
+
+bottomFrame =  Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text = "Button 1", fg = "red")
+button2 = Button(topFrame, text = "Button 2", fg = "blue")
+button3 = Button(topFrame, text = "Button 3", fg = "yellow")
+button4 = Button(bottomFrame, text = "Button 4", fg = "green")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=BOTTOM)
+
+
+
+root.mainloop()
