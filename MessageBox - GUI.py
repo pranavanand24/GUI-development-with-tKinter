266840fac5d74e3010aa06a3d21accf3d2c02524
Pranav Anand <42691222+pranavanand24@users.noreply.github.com>
2020-01-06 01:20:56 +0530

    Add files via upload

---
+++ MessageBox - GUI.py
@@ -0,0 +1,14 @@
+from Tkinter import *
+import Tkinter.messagebox
+
+root =Tk()
+Tkinter.Messagebox.showinfo('Linux Title','apes can live upto 300 years')
+
+answer = Tkinter.messagebox.askquestion('Question 1', 'Do you like silly faces')
+
+if answer == "yes":
+    print('8==d=')
+    
+root.mainloop()
+
+
